5b7b1e2a0b9ab3e9dfe2e986cd2e189d155e9366
Dantts <gabrielteixeiradantas@hotmail.com>
2023-02-12 17:36:17 -0300

    feat: car card

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,7 +3,18 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 import { Container, Header, HeaderContent, TotalCars } from './styles';
+
+const carData = {
+  brand: "Audi",
+  name: "RS 5 Coupé",
+  thumbnail: 'https://media.ed.edmunds-media.com/audi/rs-5/2018/oem/2018_audi_rs-5_coupe_quattro_fq_oem_1_1600.jpg',
+  rent: {
+    period: "Ao dia",
+    price: 120
+  }
+}
 
 export const Home = () => {
   return (
@@ -25,6 +36,8 @@ export const Home = () => {
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carData} />
     </Container>
   )
 }
