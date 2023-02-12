2cee3f110c5c97eb3fd8bbc8272ad545a9657c35
Dantts <gabrielteixeiradantas@hotmail.com>
2023-02-12 14:24:51 -0300

    feat: header and modules config

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,14 +1,31 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
 
   background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
-export const Text = styled.Text`
-  font-size: 30px;
+export const Header = styled.View`
+  width: 100%;
+  height: 113px;
+  background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: flex-end;
+`;
+
+export const HeaderContent = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  
+  padding: 0 24px 32px;
+`;
+
+export const TotalCars = styled.Text`
+  font-size: ${RFValue(15)}px;
+
   font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
 `;
